2bb8dee7262eeec67484ae97c18b1a5c031e471a
Erhahon Imafidon <mafiosco4@gmail.com>
2024-10-16 07:56:49 +0100

    Added eslint and prettier configuration

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['expo', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    quotes: ['error', 'single'],
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+  },
+};
