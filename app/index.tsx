93f3962b4fcced8b7b68627f5323535be6f24386
Erhahon Imafidon <mafiosco4@gmail.com>
2024-10-16 08:49:33 +0100

    Installed native and stack navigation depencies

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,15 @@
-import { Text, View } from "react-native";
+import { Text, View, ScrollView, StyleSheet } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
+    <SafeAreaProvider
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    </SafeAreaProvider>
   );
 }
