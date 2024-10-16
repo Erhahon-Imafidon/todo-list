7a04b2a4044b1952b13e964e4a7b534301b98144
Erhahon Imafidon <mafiosco4@gmail.com>
2024-10-16 11:32:58 +0100

    Created the add button

--- app/index.tsx
+++ app/index.tsx
@@ -2,6 +2,7 @@ import { Text, View, StyleSheet } from 'react-native';
 import Wrapper from '@/components/Wrapper';
 import DeleteButton from '@/components/Delete';
 import EditButton from '@/components/Edit';
+import AddButton from '@/components/AddButton';
 
 export default function Index() {
     return (
@@ -10,6 +11,7 @@ export default function Index() {
                 <Text>Edit app/index.tsx to edit this screen.</Text>
                 <DeleteButton onPress={() => console.log('Delete')} />
                 <EditButton onPress={() => console.log('Edited')} />
+                <AddButton onPress={() => console.log('Addition')} />
             </View>
         </Wrapper>
     );
---
+++ components/AddButton.tsx
@@ -0,0 +1,12 @@
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import { TouchableOpacity } from 'react-native';
+
+const AddButton = ({ onPress }: { onPress: () => void }) => {
+    return(
+        <TouchableOpacity onPress={onPress}>
+            <MaterialCommunityIcons name="plus-circle-outline" size={50} color="#55BCF6" />
+        </TouchableOpacity>
+    )
+}
+
+export default AddButton;
